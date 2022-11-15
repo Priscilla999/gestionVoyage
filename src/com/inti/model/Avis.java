0ef355c583ed7e3570614505927124eecc510220
Priscilla999 <priscilla.demetrius@gmail.com>
2022-11-15 08:32:47 +0100

    association: destination, hotel, avis

--- src/com/inti/model/Avis.java
+++ src/com/inti/model/Avis.java
@@ -1,5 +1,14 @@
 package com.inti.model;
 
 public class Avis {
-
+	long idavis; 
+	String commentaire;
+	
+	public Hotel hotel;
+	public void setHotel (Hotel hotel) {
+		this.hotel=hotel;
+	}
+	public Hotel getavis() {
+		return hotel;
+	}
 }
